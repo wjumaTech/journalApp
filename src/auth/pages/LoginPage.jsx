f19a774bfccde2a1f087c31579a31c0cc9ed4d7e
wjuma.tech@gmail.com <wjuma.finance@outlook.com>
2023-04-18 10:19:45 -0400

    Creado el LOGINPAGE, procediendo a configurar el THEME para las vistas

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,6 +1,97 @@
+import { Link as RouterLink } from "react-router-dom";
+import { Google } from "@mui/icons-material";
+import { Grid, Typography, TextField, Button, Link } from "@mui/material";
+
+
 export const LoginPage = () => {
   return ( 
-    <div>Login Page</div>
+    <Grid
+      container
+      spacing={ 0 }
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      sx={{ minHeight: '100vh', backgroundColor: 'primary.main', padding: 4 }}
+    >
+
+      <Grid item
+        className="box-shadow"
+        xs={ 3 }
+        sx={{ backgroundColor: 'white', padding: 3, borderRadius: 2}}  
+      >
+
+        <Typography variant="h5" sx={{ marginBottom: 1 }}>Login</Typography>
+
+        <form>
+          <Grid container>
+
+            <Grid item
+              xs={ 12 }
+              sx={{ mt: 2 }}
+            >
+              <TextField
+                label="Correo electronico"
+                type="email"
+                placeholder="Correo electronico"
+                fullWidth
+              >Correo electronico</TextField>
+            </Grid>
+
+            <Grid item
+              xs={ 12 }
+              sx={{ mt: 2 }}
+            >
+              <TextField
+                label="Contrasena"
+                type="password"
+                placeholder="Contrasena"
+                fullWidth
+              >Contrasena</TextField>
+            </Grid>
+
+            <Grid container 
+              spacing={ 2 }
+              sx={{ mb: 2, mt: 1}}
+            >
+              <Grid item 
+                xs={ 12 } 
+                sm={ 6 }
+              >
+                <Button
+                  variant="contained"
+                  fullWidth
+                >Login</Button>
+              </Grid>
+              <Grid item xs={ 12 } sm={ 6 }>
+                <Button
+                  variant="contained"
+                  fullWidth
+                >
+                  <Google />
+                  <Typography sx={{ ml: 1 }} >Google</Typography>
+                </Button>
+              </Grid>
+            </Grid>
+
+            <Grid 
+              container 
+              direction="row" 
+              justifyContent="end"
+            >
+              <Grid item>
+                  <Link component={ RouterLink } color="inherit" to="/auth/register">
+                    Crea una cuenta?
+                  </Link>
+              </Grid>
+            </Grid>
+
+          </Grid>
+        </form>
+
+      </Grid>
+
+
+    </Grid>
   );
 }
  
