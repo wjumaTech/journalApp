c73c58a73ad87ccb95ba98788ad069481d139acf
wjuma.tech@gmail.com <wjuma.finance@outlook.com>
2023-04-19 11:36:51 -0400

    Concluida la JournalPage con el Sidebar Navbar

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,9 +1,14 @@
 import { Typography } from "@mui/material";
 
+import { JournalLayout } from '../layout/JournalLayout'; 
+
 export const JournalPage = () => {
   return ( 
+    <JournalLayout>
 
-    <Typography variant="h1" >Journal Page</Typography>
+      <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, sit? Sunt eveniet, tenetur sit, assumenda praesentium ipsum ipsam magnam molestias iure ullam voluptate explicabo incidunt inventore unde cum rem eius?</Typography>
+
+    </JournalLayout>
   );
-}
+} 
  
